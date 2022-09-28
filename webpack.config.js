95d0e81fe399ef39d696a2b25644c245c2b7557f
plumchang <yuhearts.gobl@gmail.com>
2022-09-28 23:25:12 +0900

    webpackの導入

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+  devServer: {
+    static: [
+      {
+        directory: path.resolve(__dirname, "dist"),
+        publicPath: "/dist",
+      },
+      {
+        directory: path.resolve(__dirname),
+        publicPath: "/",
+      },
+    ],
+  },
+  devtool: "eval",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
