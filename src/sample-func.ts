de152a63e9ed133d7511afed102f8c587cdbb321
plumchang <yuhearts.gobl@gmail.com>
2022-10-03 20:17:27 +0900

    モジュール依存関係のsample追加

--- src/sample-func.ts
+++ src/sample-func.ts
@@ -1,3 +1,6 @@
+import { sampleFunc2 } from "./sample-func2";
+
 export function sampleFunc() {
   console.log("やぁ");
+  sampleFunc2();
 }
---
+++ src/sample-func2.ts
@@ -0,0 +1,3 @@
+export function sampleFunc2() {
+  console.log("Hi");
+}
